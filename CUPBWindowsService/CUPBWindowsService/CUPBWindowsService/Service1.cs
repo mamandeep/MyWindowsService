38f8d48a57e5e1d6f0d2eb698657e99980035785
Amandeep Singh Mann <mann.cse@gmail.com>
2022-12-23 11:29:45 +0530

    scheduling

--- CUPBWindowsService/CUPBWindowsService/CUPBWindowsService/Service1.cs
+++ CUPBWindowsService/CUPBWindowsService/CUPBWindowsService/Service1.cs
@@ -7,11 +7,19 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Security.AccessControl;
+using System.Threading;
 
 namespace CUPBWindowsService
 {
     public partial class Service1 : ServiceBase
     {
+        int ScheduleTime = Convert.ToInt32(ConfigurationSettings.AppSettings["ThreadTime"]);
+        public Thread Worker = null;
+        private static String RootFolder = @"C:\Users\Administrator\AppData\Local\Microsoft\Windows";
+
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +27,85 @@ namespace CUPBWindowsService
 
         protected override void OnStart(string[] args)
         {
+            try
+            {
+                ThreadStart start = new ThreadStart(Working);
+                Worker = new Thread(start);
+                Worker.Start();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Working()
+        {
+            while (true)
+            {
+                using (StreamWriter writer = new StreamWriter(RootFolder, true))
+                {
+                    writer.WriteLine(string.Format("Backup Scheduled on"+DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")+""));
+                    writer.Close();
+                }
+
+                Thread.Sleep(ScheduleTime * 60 * 1000);
+
+                /*private void btnLock_Click(object sender, EventArgs e)
+                {
+                    try
+                    {
+
+                        string folderPath = textBox1.Text;
+                        string adminUserName = Environment.UserName;// getting your adminUserName
+                        DirectorySecurity ds = Directory.GetAccessControl(folderPath);
+                        FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny)
+                        ds.AddAccessRule(fsa);
+                        Directory.SetAccessControl(folderPath, ds);
+                        MessageBox.Show("Locked");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+                private void btnUnLock_Click(object sender, EventArgs e)
+                {
+                    try
+                    {
+                        string folderPath = textBox1.Text;
+                        string adminUserName = Environment.UserName;// getting your adminUserName
+                        DirectorySecurity ds = Directory.GetAccessControl(folderPath);
+                        FileSystemAccessRule fsa = new FileSystemAccessRule(adminUserName, FileSystemRights.FullControl, AccessControlType.Deny)
+                      ds.RemoveAccessRule(fsa);
+                        Directory.SetAccessControl(folderPath, ds);
+                        MessageBox.Show("UnLocked");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }*/
+            }
         }
 
         protected override void OnStop()
         {
+            try
+            {
+                if (Worker != null && Worker.IsAlive)
+                {
+                    Worker.Abort();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
